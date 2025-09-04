9277762a21995293a137fd2e76597911427a455e
adhukka477 <104961039+adhukka477@users.noreply.github.com>
2025-09-04 09:25:23 -0500

    remove env variables

--- src/postgres_conn.py
+++ src/postgres_conn.py
@@ -3,23 +3,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Format: postgresql+driver://user:password@host:port/database
-user = 'postgres'
-password = 'dhukka7860'
-host = 'localhost'
-database = 'options_market_db'
+user = "postgres"
+password = os.getenv("POSTGRES_PWD")
+host = "localhost"
+database = "options_market_db"
 
 DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:5432/{database}"
 
 # Create engine
 engine = create_engine(
     DATABASE_URL,
-    echo=False,            # Log SQL statements (set False in prod)
-    pool_size=10,         # Number of connections in pool
-    max_overflow=20,      # Extra connections beyond pool_size
-    pool_timeout=30,      # Timeout for getting connection
-    pool_recycle=1800     # Recycle connections (seconds)
+    echo=False,  # Log SQL statements (set False in prod)
+    pool_size=10,  # Number of connections in pool
+    max_overflow=20,  # Extra connections beyond pool_size
+    pool_timeout=30,  # Timeout for getting connection
+    pool_recycle=1800,  # Recycle connections (seconds)
 )
 
 # Create session factory
 Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
