701445ba5084b3ce6779c599246468484bc1c266
adhukka477 <104961039+adhukka477@users.noreply.github.com>
2026-02-11 23:28:26 -0600

    converted job to end of day

--- src/main.py
+++ src/main.py
@@ -1,146 +1,123 @@
 import pytz
-from postgres_conn import Session, engine, init_pgconn
-from models.db_models import Base, make_option_model
+from postgres_conn import Session, init_pgconn
+from models.db_models import OptionsEodHistory
 from yahoo_finance import (
-    fetch_options_chain,
-    market_close_datetime,
-    market_open_datetime,
     check_market_status,
-    available_exp_dates,
     fetch_last_price,
+    fetch_options_chain,
+    available_exp_dates,
 )
 import datetime
 from models.pydantic_models import OptionContract
 import time
+from tickers import tickers
+from sqlalchemy.dialects.postgresql import insert
+import numpy as np
 
+np.seterr(all="ignore")
 
-tickers = [
-    "^SPX",
-    "^IXIC",
-    "AAPL",
-    "AMD",
-    "AMZN",
-    "BAC",
-    "DIS",
-    "GS",
-    "INTC",
-    "IWM",
-    "JPM",
-    "MSFT",
-    "NVDA",
-    "PLTR",
-    "QQQ",
-    "SPY",
-    "TSLA",
-]
 
 init_pgconn()
-OptionsDbModels = {ticker: make_option_model(ticker) for ticker in tickers}
-Base.metadata.create_all(engine)
+
+cst = pytz.timezone("US/Central")
 
 
-def insert(ticker, data):
+def insert_options(ticker, data):
 
-    # Insert into DB
+    # Insert into DB with upsert on conflict
     with Session() as session:
-        db_model = OptionsDbModels[ticker]
-        db_objs = [
-            db_model(
-                contract_symbol=opt.contractSymbol,
-                expiry_date=opt.expiry_date,
-                option_type=opt.option_type,
-                strike=opt.strike,
-                underlying_price=opt.underlying_price,
-                last_trade_date=opt.lastTradeDate,
-                last_price=opt.lastPrice,
-                bid=opt.bid,
-                ask=opt.ask,
-                volume=opt.volume,
-                open_interest=opt.openInterest,
-                implied_volatility=opt.impliedVolatility,
-                delta=opt.delta,
-                vega=opt.vega,
-                theta=opt.theta,
-                gamma=opt.gamma,
-            )
+        rows = [
+            {
+                "symbol": str(ticker).upper(),
+                "contract_symbol": opt.contractSymbol,
+                "expiry_date": opt.expiry_date,
+                "option_type": opt.option_type,
+                "strike": opt.strike,
+                "underlying_price": opt.underlying_price,
+                "last_trade_date": opt.lastTradeDate,
+                "last_price": opt.lastPrice,
+                "bid": opt.bid,
+                "ask": opt.ask,
+                "volume": opt.volume,
+                "open_interest": opt.openInterest,
+                "implied_volatility": opt.impliedVolatility,
+                "delta": opt.delta,
+                "vega": opt.vega,
+                "theta": opt.theta,
+                "gamma": opt.gamma,
+            }
             for opt in data
         ]
 
-        session.bulk_save_objects(db_objs)
-        session.commit()
+    stmt = insert(OptionsEodHistory).values(rows)
+    stmt = stmt.on_conflict_do_update(
+        constraint="options_eod_history_unique",
+        set_={
+            "last_trade_date": stmt.excluded.last_trade_date,
+            "last_price": stmt.excluded.last_price,
+            "bid": stmt.excluded.bid,
+            "ask": stmt.excluded.ask,
+            "volume": stmt.excluded.volume,
+            "open_interest": stmt.excluded.open_interest,
+            "implied_volatility": stmt.excluded.implied_volatility,
+            "delta": stmt.excluded.delta,
+            "vega": stmt.excluded.vega,
+            "theta": stmt.excluded.theta,
+            "gamma": stmt.excluded.gamma,
+        },
+    )
+
+    session.execute(stmt)
+    session.commit()
 
 
-def log_status_update(status):
-    match status:
-        case "OPEN":
-            print("Markets Are Open!")
-        case "CLOSED":
-            print("Markets Are Closed!")
-        case "HOLIDAY":
-            print("Market Holiday!")
-        case "WEEKEND":
-            print("Markets Closed for Weekend!")
+def on_market_close_time():
+    """Check if current time is between 3:30pm and 3:31pm CST"""
+    current_time_cst = datetime.datetime.now(cst)
+    current_hour = current_time_cst.hour
+    current_minute = current_time_cst.minute
+
+    return current_hour == 15 and 30 <= current_minute <= 31
 
 
 def main():
 
-    print("Starting ETL Process")
-    previous_status = None
-    next_capture_datetime = market_open_datetime
-
     while True:
+        time.sleep(1)
+        # if ( True
+        #     # on_market_close_time()
+        #     # and check_market_status(datetime.datetime.now()) == "OPEN"
+        # ):
+        if True:
+            print(
+                f"*************  Starting ETL Process -- {datetime.datetime.now(cst)} CST  *************"
+            )
 
-        current_datetime = datetime.datetime.now()
+            for ticker in tickers:
+                options_exp_dates = available_exp_dates(ticker)
+                if options_exp_dates:
+                    options_chain = fetch_options_chain(
+                        ticker=ticker, exp_dates=options_exp_dates
+                    )
+                else:
+                    print(f"No options expiration dates found for {ticker}")
+                    continue
+                if not options_chain.empty:
 
-        market_status = check_market_status(current_datetime)
-        if market_status != previous_status:
-            log_status_update(market_status)
-        previous_status = market_status
-
-        if current_datetime.time() >= market_close_datetime.time():
-            next_capture_datetime = market_open_datetime
-
-        if market_status == "OPEN":
-            if (
-                current_datetime.time() >= next_capture_datetime.time()
-                and current_datetime.time() <= market_close_datetime.time()
-            ):
-                print(f"{current_datetime} - Fetching Data")
-                for ticker in tickers:
-                    options_exp_dates = available_exp_dates(ticker)
-                    if options_exp_dates:
-                        options_chain = fetch_options_chain(
-                            ticker=ticker, exp_dates=options_exp_dates
-                        )
-                    else:
-                        continue
-                    if not options_chain.empty:
-
+                    validated_chain = []
+                    underlying_price = fetch_last_price(ticker)
+                    for x in options_chain.to_dict(orient="records"):
                         try:
-                            last_price = fetch_last_price(ticker)
+                            x["underlying_price"] = underlying_price
+                            validated_chain.append(OptionContract(**x))
                         except:
                             continue
-
-                        options_chain["underlying_price"] = last_price
-                        validated_chain = []
-
-                        for x in options_chain.to_dict(orient="records"):
-                            try:
-                                validated_chain.append(OptionContract(**x))
-                            except:
-                                print(
-                                    f"Options Contract Validation Failed for {ticker}"
-                                )
-                                continue
-                        insert(ticker=ticker, data=validated_chain)
-                    else:
-                        continue
-
-                next_capture_datetime = next_capture_datetime + datetime.timedelta(
-                    minutes=5
-                )
-
-        time.sleep(1)
+                    insert_options(ticker=ticker, data=validated_chain)
+                    print(f"Inserted {len(validated_chain)} records for {ticker}")
+                else:
+                    print(f"No options data found for {ticker}")
+                    continue
+                time.sleep(3)
 
 
 main()
